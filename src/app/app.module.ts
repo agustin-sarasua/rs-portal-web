bfa2879920b3dc2578291fbf574c22485628028f
Agustin Sarasua <agustin.sarasua@mercadolibre.com>
2017-12-24 12:50:07 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderTopComponent } from './header/header-top/header-top.component';
+import { HeaderRecommendedComponent } from './header/header-recommended/header-recommended.component';
+import { HeaderSearchComponent } from './header/header-search/header-search.component';
+import { HeaderMenuComponent } from './header/header-menu/header-menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeaderTopComponent,
+    HeaderRecommendedComponent,
+    HeaderSearchComponent,
+    HeaderMenuComponent
   ],
   imports: [
     BrowserModule
